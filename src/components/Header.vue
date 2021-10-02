ab1e8a2379b460eeec1aa6a399ec0ef2ffcf2944
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-10-02 11:34:32 +0200

    Start project

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+      <img src="@/assets/logo.png" alt="Logo Boolgram">
+      <input type="text" placeholder="CERCA">
+      <div class="keys">
+          <i class="fas fa-home">HOME</i>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+header{
+    display:flex;
+    width:100vh;
+    justify-content:space-around;
+    align-items:center;
+    i{
+        font-size: 20px;
+        color: green;
+    }
+
+}
+</style>
